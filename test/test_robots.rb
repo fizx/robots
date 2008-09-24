2e137436609e0a374d37b407fb218a1db946979f
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2008-09-23 18:45:34 -0700

    test stub

---
+++ test/test_robots.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require "test/unit"
+require File.dirname(__FILE__) + "/../lib/robots"
+
+class TestRobots < Test::Unit::TestCase
+  
+  def test_truth
+    assert true
+  end
+end
